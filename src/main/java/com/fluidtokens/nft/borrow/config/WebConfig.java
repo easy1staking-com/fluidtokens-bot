202f0be20da772acbc08328775f340c2cc222daf
Giovanni Gargiulo <gargiulo.gianni@gmail.com>
2024-08-15 11:17:45 +0100

    chore: added missing files

---
+++ src/main/java/com/fluidtokens/nft/borrow/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.fluidtokens.nft.borrow.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+
+}
